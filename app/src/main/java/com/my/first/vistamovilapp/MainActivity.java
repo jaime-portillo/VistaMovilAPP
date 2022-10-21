7382c0524d46410352bc11d1454addfbbd44a487
Jaime Portillo <jaime.portillo@catolica.edu.sv>
2022-10-21 00:33:41 -0600

    submenu cotizacion

--- app/src/main/java/com/my/first/vistamovilapp/MainActivity.java
+++ app/src/main/java/com/my/first/vistamovilapp/MainActivity.java
@@ -24,6 +24,6 @@ public class MainActivity extends AppCompatActivity {
         };
 
         Timer tiempo= new Timer();
-        tiempo.schedule(tarea,3000);
+        tiempo.schedule(tarea,1000);
     }
 }
